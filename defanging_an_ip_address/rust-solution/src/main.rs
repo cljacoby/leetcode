8e8aa6e8652befbb9f2983d8a25f5612858ce59d
Chris Jacoby <chris@Chriss-MBP.fios-router.home>
2020-01-07 17:57:27 -0500

    Starting new problem: convert_binary_number_in_a_linked_list_to_integer

--- defanging_an_ip_address/rust-solution/src/main.rs
+++ defanging_an_ip_address/rust-solution/src/main.rs
@@ -1,17 +1,21 @@
-struct Solution{}
+struct Solution {}
 
 impl Solution {
-
-    // The signature is spelt wrong on leetcode; change to this when submitting:
-    // pub fn defang_i_paddr(address: String) -> String
+    // The signature is spelt wrong on leetest_caseode; change to this when submitting:
+    //      pub fn defang_i_paddr(address: String) -> String
     pub fn defang_ip_addr(address: String) -> String {
         return address.replace(".", "[.]");
     }
-
 }
 
 fn main() {
-    println!("main");
+    let input = "1.1.1.1";
+    let output = "1[.]1[.]1[.]1";
+    let result = Solution::defang_ip_addr(String::from(input));
+    println!(
+        "input = '{}'\noutput = '{}'\nresult = '{}'\n",
+        input, output, result
+    );
 }
 
 #[cfg(test)]
@@ -20,6 +24,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn test() {
+        let test_cases = vec![
+            ("", ""),
+            (".", "[.]"),
+            ("1.1.1.1", "1[.]1[.]1[.]1"),
+            ("255.100.", "255[.]100[.]"),
+            ("255_100_50_0", "255_100_50_0"),
+        ];
 
+        for (input, output) in test_cases.iter() {
+            println!("input = {}, output = {}", input, output);
+            let result = Solution::defang_ip_addr(String::from(*input));
+            assert_eq!(result, String::from(*output));
+        }
+    }
 }
