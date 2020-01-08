fe22e28b111cb2ae6e39b878f698172069ae47c0
Chris Jacoby <chris@Chriss-MBP.fios-router.home>
2020-01-07 20:16:27 -0500

    Finishefd all the helper test code im going to write for a random leetcode question

--- convert_binary_number_in_a_linked_list_to_integer/convert_binary_number_in_a_linked_list_to_integer.py
+++ convert_binary_number_in_a_linked_list_to_integer/convert_binary_number_in_a_linked_list_to_integer.py
@@ -48,6 +48,26 @@ class Solution:
 
 
 class TestSolution(ut.TestCase):
+
+    def controls(self):
+        n1_0 = ListNode(0)
+        n1_1 = ListNode(1)
+        n1_2 = ListNode(2)
+        n2_0 = ListNode(0)
+        n2_1 = ListNode(1)
+        n2_2 = ListNode(2)
+        n2_3 = ListNode(3)
+        n1_0.next = n1_1
+        n1_0.next.next = n1_2
+        n2_0.next = n2_1
+        n2_0.next.next = n2_2
+        n2_0.next.next.next = n2_3
+        return [
+            (n1_0, [0, 1, 2]),
+            (n2_0, [0, 1, 2, 3]),
+        ]
+
+
     def test_list_to_ll_negatives(self):
         """Test negative cases for method list_to_ll"""
         neg_tests = [Ns(input=[], output=None)]
@@ -60,19 +80,15 @@ class TestSolution(ut.TestCase):
 
     def test_list_to_ll_positives(self):
         """Test positive cases for method list_to_ll"""
-        # head_1 = ListNode(1)
-        # head_1.next = ListNode(2)
-        # head_1.next.next = ListNode(3)
-        # pos_tests = [Ns(input=[1, 2, 3], output=head_1)]
-        # for case in pos_tests:
-        #     index = 0
-        #     node = Solution.list_to_ll(case.input)
-        #     a_node = case.output
-        #     while node != None:
-        #         self.assertEqual(case.input[index], node.val)
-        #         node = node.next
-        #         index += 1
-        #     self.assertEqual(index, len(case.input))
+        for ctl_head, ctl_arr in self.controls():
+            # Must pass copy as implementation pops from given list
+            head = Solution.list_to_ll(ctl_arr.copy())
+            ctl_node, node = ctl_head, head
+            index = 0
+            while ctl_node.next != None:
+                self.assertEqual(ctl_node.val, node.val, ctl_arr[index])
+                index += 1
+                ctl_node, node = ctl_node.next, node.next
 
     # =========================================================================
     # =========================================================================
