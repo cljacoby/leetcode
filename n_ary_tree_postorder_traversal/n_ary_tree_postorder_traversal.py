6ffcca630c9d14a2c039ece989fbc714bdf8fff4
cljacoby <cljacoby@gmail.com>
2020-05-31 19:30:56 -0400

    Finished n_ary_tree_postorder_traversal

---
+++ n_ary_tree_postorder_traversal/n_ary_tree_postorder_traversal.py
@@ -0,0 +1,54 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    # def postorder(self, root: 'Node') -> List[int]:
+    def postorder(self, root): 
+        arr = []
+        if root != None:
+            self.postorder_recr(root, arr)
+        return arr
+
+    def postorder_recr(self, root, arr):
+        if root.children != None:
+            for child in root.children:
+                self.postorder_recr(child, arr)
+        arr.append(root.val)
+
+    def postorder_iter(self, root):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    """
+             1
+          /  |  \ 
+        2    3    4
+      /  \ 
+    5     6
+    """
+
+    root = Node(1, [
+         Node(2, [
+             Node(5),
+             Node(6),
+         ]),
+         Node(3),
+         Node(4),
+    ])
+
+
+    result = sol.postorder(root)
+    print(f"result = {result}")
+
+
+
+    root2 = None
+    result = sol.postorder(root2)
+    print(f"result = {result}")
+
---
+++ n_ary_tree_postorder_traversal/n_ary_tree_postorder_traversal/src/main.rs
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+fn main() {
+    println!("n_ary_tree_postorder_traversal/n_ary_tree_postorder_traversal");
+}
