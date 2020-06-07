169390dd14647342247cd0c41d1a90cf1c4c015e
cljacoby <cljacoby@gmail.com>
2020-06-07 18:38:10 -0400

    finished max-area-of-island

--- max_area_of_island/max_area_of_island/src/main.rs
+++ max_area_of_island/max_area_of_island/src/main.rs
@@ -1,76 +1,38 @@
 struct Solution {}
 
 impl Solution {
-    pub fn neighbors(coord: (usize, usize), grid: &Vec<Vec<i32>>) -> Vec<(usize, usize)> {
-        let (x, y) = coord;
-        let mut neighbors = Vec::with_capacity(4);
-
-        // can go up?
-        if y > 0 {
-            neighbors.push((x, y - 1));
-        }
-        // can go right?
-        if x < grid[0].len() - 1 {
-            neighbors.push((x + 1, y));
-        }
-        // can go down?
-        if y < grid.len() - 1 {
-            neighbors.push((x, y + 1));
-        }
-        // can go left?
-        if x > 0 {
-            neighbors.push((x - 1, y));
-        }
-
-        neighbors
-    }
-
-    pub fn max_island(coord: (usize, usize), grid: &Vec<Vec<i32>>, area: i32) {
-        let n_coords: Vec<(usize, usize)> = Solution::neighbors(coord, grid)
-            .into_iter()
-            .filter(|(x, y)| grid[*x][*y] == 1)
-            .filter(|(x, y)| !(*x == coord.0 && *y == coord.1))
-            .collect();
-
-        println!("n_coords = {:?}", n_coords);
-        for n_coord in n_coords {
-            println!(
-                "coord = {:?}, area = {:?}, n_coord = {:?}",
-                coord, area, n_coord
-            );
-            Solution::max_island(n_coord, grid, area + 1);
-        }
-    }
-
-    pub fn _max_area_of_island(grid: Vec<Vec<i32>>) -> i32 {
-        for (i, row) in grid.iter().enumerate() {
-            for (j, el) in row.iter().enumerate() {
-                let neighbors = Solution::neighbors((i, j), &grid);
-                Solution::max_island((i, j), &grid, 1);
-            }
-        }
-
-        0
-    }
-
+   
     pub fn area(row: usize, col: usize, grid: &Vec<Vec<i32>>, seen: &mut Vec<Vec<bool>>) -> i32 {
-        println!("row = {:?}, col = {:?}", row, col);
-        if (row < 0
-            || row > grid.len()
+        // Check coordinate in grid
+        // Check grid doesn't equal 0
+        // Checl not already seen cooordinate
+        if row < 0
             || col < 0
-            || col > grid[0].len()
+            || row >= grid.len()
+            || col >= grid[0].len()
+            || grid[row][col] == 0
             || seen[row][col]
-            || grid[row][col] == 0)
         {
             return 0;
         }
 
-        seen[row][col] == true;
+        // This is valid coordinate. Add to seen coordinates for future recursions.
+        seen[row][col] = true;
 
-        Solution::area(row + 1, col, grid, seen)
-            + Solution::area(row - 1, col, grid, seen)
-            + Solution::area(row, col + 1, grid, seen)
-            + Solution::area(row, col - 1, grid, seen)
+        // Both `row` and `col` are type usize. Producing a negative value for usize is a
+        // bounds overflow for the type. Therefore, check the value before executing
+        // leftward and downward board motion to prevent overflow.
+        let mut area = 1;
+        area += Solution::area(row, col + 1, grid, seen);
+        area += Solution::area(row + 1, col, grid, seen);
+        if col > 0 {
+            area += Solution::area(row, col - 1, grid, seen);
+        }
+        if row > 0 {
+            area += Solution::area(row - 1, col, grid, seen);
+        }
+
+        area
     }
 
     pub fn max_area_of_island(grid: Vec<Vec<i32>>) -> i32 {
@@ -90,17 +52,22 @@ impl Solution {
 }
 
 fn main() {
-    let grid: Vec<Vec<i32>> = vec![
-        vec![0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        vec![0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-        vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    ];
+    // let grid: Vec<Vec<i32>> = vec![
+    //     vec![0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    //     vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    //     vec![0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+    //     vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+    //     vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    //     vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    //     vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    // ];
 
+    let grid: Vec<Vec<i32>> = vec![
+        vec![0, 1,],
+        vec![1, 1,],
+    ];
+    
     let area = Solution::max_area_of_island(grid);
     println!("area = {:?}", area);
 
