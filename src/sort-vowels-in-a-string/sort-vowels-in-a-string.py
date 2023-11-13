eda6b5e670ce85310b6014f8b3a95528142f9cff
cljacoby <cljacoby@gmail.com>
2023-11-13 18:24:20 -0500

    Additional solution for sort-vowels-in-a-string

--- src/sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ src/sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -26,6 +26,34 @@ class Solution(object):
             char = heappop(v)
             s[i] = char
         return "".join(s)
+
+# No heap or deque. Just use regular list and reverse sort before
+# second loop.
+class Solution(object):
+    def sortVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = set([
+            'a', 'e', 'i', 'o', 'u',
+            'A', 'E', 'I', 'O', 'U',
+        ])
+        idx = []
+        v = []
+        s = [i for i in s]
+        for i, char in enumerate(s):
+            if char in vowels:
+                idx.append(i)
+                v.append(char)
+        v.sort(reverse=True)
+        idx = list(reversed(idx))
+        while len(v) > 0 and len(idx) > 0:
+            assert len(v) == len(idx), "Queue length mismatch"
+            i = idx.pop()
+            char = v.pop()
+            s[i] = char
+        return "".join(s)
         
 
 if __name__ == "__main__":
