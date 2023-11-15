48c6acabda4b654f4d7ab951a21282181126d40b
cljacoby <cljacoby@gmail.com>
2023-11-14 22:27:31 -0500

    Solve unique-length-3-palindromic-subsequences python

--- README.md
+++ README.md
@@ -120,3 +120,4 @@
 | :white_check_mark: | 725    | Python      | [split-linked-list-in-parts](src/split-linked-list-in-parts)                                                               | Medium     | Linked List         |
 | :white_check_mark: | 138    | Python      | [copy-list-with-random-pointer](src/copy-list-with-random-pointer)                                                         | Medium     | Linked List         |
 | :white_check_mark: | 2785   | Python      | [sort-vowels-in-a-string](src/sort-vowels-in-a-string)                                                                     | Medium     | Heap / Array        |
+| :white_check_mark: | 1930   | Python      | [unique-length-3-palindromic-subsequences](src/unique-length-3-palindromic-subsequences)                                   | Medium     | Array               |
---
+++ src/unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,52 @@
+# https://leetcode.com/problems/unique-length-3-palindromic-subsequences
+
+'''
+Had to reference others solutions on this one. I had the right idea of
+building the index map. Where I went wrong was trying to iterate over
+the first have of characters up to a midpoint testing each for a
+palindrome.
+
+The key insight was recognizing any 3 length palindrome will be specific
+to characters with a count >= 2. Therefore, we can take all of these
+instanceas, take the first and last index, slice the string, and count
+the unique characters occuring within. Summing these together yields the
+answer.
+'''
+
+class Solution(object):
+    def countPalindromicSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        idxmap = dict()
+        for i, char in enumerate(s):
+            if char not in idxmap:
+                idxmap[char] = [i]
+            else:
+                idxmap[char].append(i)
+        count = 0
+        for char, idx in idxmap.items():
+            if len(idx) < 2:
+                continue
+            start = idx[0]
+            end = idx[-1]
+            sliced = s[start+1:end]
+            uniques = set(sliced)
+            count += len(uniques)
+        return count
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = [
+        ("aabca", 3),
+        ("bbcbaba", 4),
+        ("aywvhbwycmbttdmogwlfosfizqlndfipffbqfxwbgrfdyomuuecllmsrzckiwgelkhgylwobz", 326),
+        (open("test1.txt").read(), 676)
+    ]
+    for (s, solution) in tests:
+        result = sol.countPalindromicSubsequence(s)
+        assert result == solution, \
+            f"result {result} != solution {solution}"
+    print("✅ All tests passed")
+
