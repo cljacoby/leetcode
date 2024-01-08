8d294f0194d03658df4ae83929335fc9cf3752cf
cljacoby <cljacoby@gmail.com>
2024-01-08 09:27:29 -0500

    Add simple shell script for tracking stats

---
+++ stats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Total:"
+cat README.md \
+    | grep ':white_check_mark:' \
+    | wc -l
+echo ""
+
+echo "Difficulties:"
+cat README.md \
+    | grep -i ':white_check_mark:' \
+    | grep -Eo '(Easy|Medium|Hard)' \
+    | sort \
+    | uniq -c
+echo ""
+
+echo "Categories:"
+cat README.md \
+    | tail -n +3 \
+    | awk -F "|" '{print $7 }' \
+    | sort \
+    | uniq -c \
+    | sort -r
+echo ""
